a4c8b7eb6d3021ac3f83026e7b12aa62adbd0b86
mohamad <mohamad.fathi13811@gmail.com>
2024-07-24 17:55:35 +0330

    register user and make form etc...

--- website/views.py
+++ website/views.py
@@ -1,8 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.urls import reverse
+from . import forms
 
 
 def home(request):
-    context = {
+    # check to see of logging in
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        # authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "YOU HAVE LOGGED IN!")
+            return redirect(reverse('home'))
+        else:
+            messages.error(request, "THERE WAS AN ERROR THERE. PLEASE TRY AGAIN...!")
+            return redirect(reverse('home'))
+    else:
+        return render(request, 'website/home.html', {})
 
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "YOU HAVE LOGGED OUT...!")
+    return redirect(reverse('home'))
+
+
+def register_user(request):
+    register_form = forms.SignUpForm()
+    context = {
+        'register_form': register_form
     }
-    return render(request, 'website/home.html', context)
+    return render(request, 'website/register.html', context)
